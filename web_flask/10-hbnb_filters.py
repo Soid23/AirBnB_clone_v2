84c0f0196647f349e29e4fdea532335bae7ef4f4
Dorris Mutahi <dorrismutahi@gmail.com>
2024-04-20 21:58:26 +0300

    copying files

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Starts flask application HBNB filters
+"""
+
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html", states=states,
+                           amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(exec):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
